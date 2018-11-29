b652cbddf14435e35093101aacccc239054a0542
KelseyMorgue <kelseymorgue666@gmail.com>
2018-11-29 14:58:38 -0700

    view controller goals
    added to new run and run detail view controllers what the end goal is
    of each.

--- RunTrax/NewRunViewController.swift
+++ RunTrax/NewRunViewController.swift
@@ -19,129 +19,20 @@ class NewRunViewController: UIViewController {
     @IBOutlet weak var paceLabel: UILabel!
     @IBOutlet weak var endButton: UIButton!
     
-    private var run: Run?
-    private let locationManager = LocationManager.shared
-    private var seconds = 0
-    private var timer: Timer?
-    private var distance = Measurement(value: 0, unit: UnitLength.meters)
-    private var locationList: [CLLocation] = []
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        timer?.invalidate()
-        locationManager.stopUpdatingLocation()
-    }
-    
-    func eachSecond() {
-        seconds += 1
-        updateDisplay()
-    }
-    
-    private func updateDisplay() {
-        let formattedDistance = FormatDisplay.distance(distance)
-        let formattedTime = FormatDisplay.time(seconds)
-        let formattedPace = FormatDisplay.pace(distance: distance,
-                                               seconds: seconds,
-                                               outputUnit: UnitSpeed.minutesPerMile)
-        
-        distanceLabel.text = "Distance:  \(formattedDistance)"
-        timeLabel.text = "Time:  \(formattedTime)"
-        paceLabel.text = "Pace:  \(formattedPace)"
-    }
-    
-    private func startRun() {
-        seconds = 0
-        distance = Measurement(value: 0, unit: UnitLength.meters)
-        locationList.removeAll()
-        updateDisplay()
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-            self.eachSecond()
-        }
-        startLocationUpdates()
-        
-    }
-    
-    private func stopRun() {
-        locationManager.stopUpdatingLocation()
-    }
-    
-    private func saveRun() {
-        let newRun = Run(context: CoreDataStack.context)
-        newRun.distance = distance.value
-        newRun.duration = Int16(seconds)
-        newRun.timestamp = Date()
-        
-        for location in locationList {
-            let locationObject = Location(context: CoreDataStack.context)
-            locationObject.timestamp = location.timestamp
-            locationObject.latitude = location.coordinate.latitude
-            locationObject.longitude = location.coordinate.longitude
-            newRun.addToLocations(locationObject)
-        }
-        
-        CoreDataStack.saveContext()
-        
-        run = newRun
-    }
-    @IBAction func stopTapped() {
-        let alertController = UIAlertController(title: "Run Completed",
-                                                message: "Would you like to save or discard your run?",
-                                                preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        alertController.addAction(UIAlertAction(title: "Save", style: .default) { _ in
-            self.stopRun()
-            self.saveRun() // ADD THIS LINE!
-            self.performSegue(withIdentifier: .details, sender: nil)
-        })
-        
-        alertController.addAction(UIAlertAction(title: "Discard", style: .destructive) { _ in
-            self.stopRun()
-            _ = self.navigationController?.popToRootViewController(animated: true)
-        })
-        
-        present(alertController, animated: true)
-        
-    }
-    private func startLocationUpdates() {
-        locationManager.delegate = self
-        locationManager.activityType = .fitness
-        locationManager.distanceFilter = 10
-        locationManager.startUpdatingLocation()
-    }
+   
+    //What to do in this view controller:
+    //  -Have distance, time, and pace update and displayed
+    //  -Show map with current path (?? need to look up)
+    //  -When end button is clicked:
+    //      *Have save/delete option pop up
+    //      *If run is saved, push to next screen (sharing run options, and totals)
+    //      *If run is deleted, return to home screen
+
     
     
 }
 
-extension NewRunViewController: SegueHandlerType {
-    enum SegueIdentifier: String {
-        case details = "RunDetailsViewController"
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        switch segueIdentifier(for: segue) {
-        case .details:
-            let destination = segue.destination as! RunDetailsViewController
-            destination.run = run
-        }
-    }
-}
-extension NewRunViewController: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        for newLocation in locations {
-            let howRecent = newLocation.timestamp.timeIntervalSinceNow
-            guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
-            
-            if let lastLocation = locationList.last {
-                let delta = newLocation.distance(from: lastLocation)
-                distance = distance + Measurement(value: delta, unit: UnitLength.meters)
-            }
-            
-            locationList.append(newLocation)
-        }
-    }
-    
-}
+
