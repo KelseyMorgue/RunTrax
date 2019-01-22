9433cc52dc672619b80c2c054365ed4a206683ad
KelseyMorgue <kelseymorgue666@gmail.com>
2019-01-22 11:39:38 -0700

    Fixed bugs
    Caused from playing around understanding different techniques

--- RunTrax/NewRunViewController.swift
+++ RunTrax/NewRunViewController.swift
@@ -2,37 +2,54 @@
 //  NewRunViewController.swift
 //  RunTrax
 //
-//  Created by Kelsey Henrichsen on 11/21/18.
-//  Copyright © 2018 Kelsey Henrichsen. All rights reserved.
-//
+//  Created by Kelsey Henrichsen on 1/22/19.
+//  Copyright © 2019 Kelsey Henrichsen. All rights reserved.
 
-import Foundation
-import UIKit
 import MapKit
 import CoreLocation
+import Foundation
 
-class NewRunViewController: UIViewController {
-    //Properties
+class NewRunViewController: UIViewController
+{
+    //properties
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var paceLabel: UILabel!
-    @IBOutlet weak var endButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
+    private var run: Run?
+    private let locationManager = LocationManager.shared
+    private var seconds = 0
+    private var timer: Timer?
+    private var distance = Measurement(value: 0, unit: UnitLength.meters)
+    private var locationList: [CLLocation] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-   
-    //What to do in this view controller:
-    //  -Have distance, time, and pace update and displayed
-    //  -Show map with current path (?? need to look up)
-    //  -When end button is clicked:
-    //      *Have save/delete option pop up
-    //      *If run is saved, push to next screen (sharing run options, and totals)
-    //      *If run is deleted, return to home screen
+    
+    func eachSecond() {
+        seconds += 1
+        updateDisplay()
+    }
+    
+    private func updateDisplay() {
+        let formattedDistance = FormatDisplay.distance(distance)
+        let formattedTime = FormatDisplay.time(seconds)
+        let formattedPace = FormatDisplay.pace(distance: distance,seconds: seconds, outputUnit: UnitSpeed.minutesPerMile)
+        
+        distanceLabel.text = "Distance:  \(formattedDistance)"
+        timeLabel.text = "Time:  \(formattedTime)"
+        paceLabel.text = "Pace:  \(formattedPace)"
+    }
 
-    
-    
 }
 
-
+//What to do in this view controller:
+//  -Have distance, time, and pace update and displayed
+//  -Show map with current path (?? need to look up)
+//  -When end button is clicked:
+//      *Have save/delete option pop up
+//      *If run is saved, push to next screen (sharing run options, and totals)
+//      *If run is deleted, return to home screen
