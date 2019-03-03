183c488dcd3b2630f71d7144db232fd60d57c44d
KelseyMorgue <kelseymorgue666@gmail.com>
2019-03-03 13:46:10 -0700

    Adjustments to various files
    played with navigation controllers, added images into proper folder
    (then changed them to match in storyboard), updated info p list for
    bundle identifier (matchy matchy)

--- RunTrax/LoginViewController.swift
+++ RunTrax/LoginViewController.swift
@@ -16,11 +16,21 @@ class LoginViewController:UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-        
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//
+//        self.emailField.delegate = self
+//        self.passwordField.delegate = self
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        self.view.endEditing(true)
+//        return false
+//    }
 
 }
