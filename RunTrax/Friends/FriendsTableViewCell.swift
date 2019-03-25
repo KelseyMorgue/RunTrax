4628868b4c7556040fec8b3950a6056370665d86
KelseyMorgue <kelseymorgue666@gmail.com>
2019-03-25 15:28:21 -0600

    add button fixed and username format fixed

--- RunTrax/Friends/FriendsTableViewCell.swift
+++ RunTrax/Friends/FriendsTableViewCell.swift
@@ -69,27 +69,6 @@ class FriendsTableViewCell: UITableViewCell {
         }
     }
     
-    func loadCurrentFriends()
-    {
-        ref.child("users").child(userID?.uid ?? "derp").child("friends").observeSingleEvent(of: .value, with: { (snapshot) in
-            // Get user value
-            if self.friendItem != nil
-            {
-                self.friendsUsername.text = self.friendItem.name
-                
-                //TODO: query on user, for the friends that equals that id
-                let storageRef = self.storage.reference(withPath: "profile_images/\(self.friendItem?.id ?? "derp")/userImage.png")
-                let placeHolderImage = UIImage(named: "default")
-                self.friendsProfilePicture.sd_setImage(with: storageRef, placeholderImage: placeHolderImage)
-                
-                
-                
-            }
-        }) { (error) in
-            print("hello error")
-            print(error.localizedDescription)
-        }
-    }
     
     //adds new friend to user's friendlist
     @IBAction func addButtonClicked(_ sender: UIButton) {
